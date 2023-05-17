2aee361e70e38f748826ce34c554d0ffc3f41a5d
t-onyango <t.onyango@alustudent.com>
2023-05-17 21:58:29 +0300

    update the solution

--- challenge1-password-generator/finalPassowrd-gen.py
+++ challenge1-password-generator/finalPassowrd-gen.py
@@ -35,7 +35,7 @@ def generate_password(length=8):
         return password
 
     # Returns a text if validation fails
-    return "Invalid password, validation fails!"
+    return "Invalid password, validation fails. Rerun the program!"
 
 # calling the generate_password function with the provided length, and the resulting password 
 # is stored in the password variable.
